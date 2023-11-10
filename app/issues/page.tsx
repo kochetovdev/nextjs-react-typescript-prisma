c2485a586f8120687eb55f56f4c3d4e00d6665d3
kochetovdev <kochetov.yevhen.dev@gmail.com>
2023-11-10 18:27:34 +0200

    Handle form submision

--- app/issues/page.tsx
+++ app/issues/page.tsx
@@ -3,7 +3,13 @@ import Link from "next/link";
 import React from "react";
 
 const IssuesPage = () => {
-  return <Button><Link href='/issues/new'>New issue</Link></Button>;
+  return (
+    <div className="p-5">
+      <Button>
+        <Link href="/issues/new">New issue</Link>
+      </Button>
+    </div>
+  );
 };
 
 export default IssuesPage;
