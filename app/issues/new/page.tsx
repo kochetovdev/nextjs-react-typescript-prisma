93915631b5dd38e4f2982121abae63493772a51a
kochetovdev <kochetov.yevhen.dev@gmail.com>
2023-11-10 17:35:11 +0200

    build the new Issue page

---
+++ app/issues/new/page.tsx
@@ -0,0 +1,18 @@
+"use client";
+
+import { Button, TextArea, TextField } from "@radix-ui/themes";
+import React from "react";
+
+const NewIssuePage = () => {
+  return (
+    <div className="max-w-xl space-y-3 p-5">
+      <TextField.Root>
+        <TextField.Input placeholder="title" />
+      </TextField.Root>
+      <TextArea placeholder="description" />
+      <Button>Submit new issue</Button>
+    </div>
+  );
+};
+
+export default NewIssuePage;
--- app/issues/page.tsx
+++ app/issues/page.tsx
@@ -1,8 +1,9 @@
 import { Button } from "@radix-ui/themes";
+import Link from "next/link";
 import React from "react";
 
 const IssuesPage = () => {
-  return <Button>New issue</Button>;
+  return <Button><Link href='/issues/new'>New issue</Link></Button>;
 };
 
 export default IssuesPage;
