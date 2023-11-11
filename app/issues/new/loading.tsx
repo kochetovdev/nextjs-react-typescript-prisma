1a6777e521dcc43277c5cfb299106adf9ca21688
kochetovdev <kochetov.yevhen.dev@gmail.com>
2023-11-11 17:38:00 +0200

    show issue details

---
+++ app/issues/new/loading.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const LoadingNewIssue = () => {
+  return (
+    <div>Loading...</div>
+  )
+}
+
+export default LoadingNewIssue
--- app/issues/page.tsx
+++ app/issues/page.tsx
@@ -3,12 +3,12 @@ import { Button, Table } from "@radix-ui/themes";
 import Link from "next/link";
 import React from "react";
 import IssueStatusBage from "../components/IssueStatusBage";
-import delay from 'delay';
+import delay from "delay";
 import IssueActons from "./IssueActons";
 
 const IssuesPage = async () => {
   const issues = await prisma.issue.findMany();
-  await delay(2000)
+  await delay(2000);
 
   return (
     <div className="p-5">
@@ -17,19 +17,29 @@ const IssuesPage = async () => {
         <Table.Header>
           <Table.Row>
             <Table.ColumnHeaderCell>Issue</Table.ColumnHeaderCell>
-            <Table.ColumnHeaderCell className="hidden md:table-cell">Status</Table.ColumnHeaderCell>
-            <Table.ColumnHeaderCell className="hidden md:table-cell">Created</Table.ColumnHeaderCell>
+            <Table.ColumnHeaderCell className="hidden md:table-cell">
+              Status
+            </Table.ColumnHeaderCell>
+            <Table.ColumnHeaderCell className="hidden md:table-cell">
+              Created
+            </Table.ColumnHeaderCell>
           </Table.Row>
         </Table.Header>
         <Table.Body>
           {issues.map((issue) => (
             <Table.Row key={issue.id}>
               <Table.Cell>
-                {issue.title}
-                <div className="block md:hidden"><IssueStatusBage status={issue.status} /></div>
+                <Link href={`issues/${issue.id}`}>{issue.title}</Link>
+                <div className="block md:hidden">
+                  <IssueStatusBage status={issue.status} />
+                </div>
               </Table.Cell>
-              <Table.Cell className="hidden md:table-cell"><IssueStatusBage status={issue.status} /></Table.Cell>
-              <Table.Cell className="hidden md:table-cell">{issue.createdAt.toDateString()}</Table.Cell>
+              <Table.Cell className="hidden md:table-cell">
+                <IssueStatusBage status={issue.status} />
+              </Table.Cell>
+              <Table.Cell className="hidden md:table-cell">
+                {issue.createdAt.toDateString()}
+              </Table.Cell>
             </Table.Row>
           ))}
         </Table.Body>
