893b1db181f9dc7e9d3eee42dcd0a101265eab6f
kochetovdev <kochetov.yevhen.dev@gmail.com>
2023-11-13 17:42:03 +0200

    Fix cahing issue

--- app/issues/_components/IssueForm.tsx
+++ app/issues/_components/IssueForm.tsx
@@ -43,6 +43,7 @@ const IssueForm = ({ issue }: { issue?: Issue }) => {
         await axios.post("/api/issues", data);
       }
       router.push("/issues");
+      router.refresh();
     } catch (error) {
       setSubmiting(false);
       setError("An unexpected error occured");
