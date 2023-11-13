8293437423a410dec17073d61a09fb5197f14e53
kochetovdev <kochetov.yevhen.dev@gmail.com>
2023-11-13 11:46:36 +0200

    Refactor: Orginized import statements

--- app/issues/[id]/page.tsx
+++ app/issues/[id]/page.tsx
@@ -1,4 +1,4 @@
-import IssueStatusBage from "@/app/components/IssueStatusBage";
+import { IssueStatusBadge } from "@/app/components";
 import prisma from "@/prisma/migrations/client";
 import { Card, Flex, Heading, Text } from "@radix-ui/themes";
 import { notFound } from "next/navigation";
@@ -23,7 +23,7 @@ const IssueDetailPage = async ({ params }: Props) => {
     <div className="px-5">
       <Heading>{issue.title}</Heading>
       <Flex className="space-x-3" my="2">
-        <IssueStatusBage status={issue.status}></IssueStatusBage>
+        <IssueStatusBadge status={issue.status}></IssueStatusBadge>
         <Text>{issue.createdAt.toDateString()}</Text>
       </Flex>
       <Card className="prose mt-4">
