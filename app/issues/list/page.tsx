9685b7e91c763f5283a6e78d457bef6d19438ce8
kochetovdev <kochetov.yevhen.dev@gmail.com>
2023-11-15 18:47:25 +0200

    Filter issues

--- app/issues/list/page.tsx
+++ app/issues/list/page.tsx
@@ -1,10 +1,24 @@
 import prisma from "@/prisma/client";
+import { Status } from "@prisma/client";
 import { Table } from "@radix-ui/themes";
 import { IssueStatusBadge, Link } from "../../components";
 import IssueActons from "./IssueActons";
 
-const IssuesPage = async () => {
-  const issues = await prisma.issue.findMany();
+const IssuesPage = async ({
+  searchParams,
+}: {
+  searchParams: { status: Status };
+}) => {
+  const statuses = Object.values(Status);
+  const status = statuses.includes(searchParams.status)
+    ? searchParams.status
+    : undefined;
+
+  const issues = await prisma.issue.findMany({
+    where: {
+      status
+    },
+  });
   return (
     <div>
       <IssueActons />
